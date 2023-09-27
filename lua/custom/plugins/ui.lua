fb680844e862663f7ee8592ec624f809bb69ea05
Martin <martin@fijal.se>
2023-09-27 17:43:03 +0200

    Testing sonokai for a bit

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -1,10 +1,20 @@
 return {
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
+  -- Colorscheme
+  {
+    'sainnhe/sonokai',
+    priority = 999,
+    config = function()
+      vim.g.sonokai_style = 'atlantis'
+      vim.cmd.colorscheme 'sonokai'
+    end,
+  },
+
   -- TODO move
   -- CtrlP
   {
-	'ctrlpvim/ctrlp.vim'
+    'ctrlpvim/ctrlp.vim'
   },
 
   -- Zen mode
