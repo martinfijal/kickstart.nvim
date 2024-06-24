73a2b7d9ebe97d165485631fd2937479537b51a1
Martin <martin@fijal.se>
2024-06-24 16:40:43 +0200

    Fix trouble and add neotree

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -1,6 +1,28 @@
 return {
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    keys = {
+      { "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "Neotree" },
+    },
+    opts = {
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["<space>"] = "none",
+        },
+      },
+    },
+  },
+
   -- Colorscheme
   {
     'sainnhe/sonokai',
